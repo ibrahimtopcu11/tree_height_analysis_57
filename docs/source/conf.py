c16969363becd374d4b26b529cdc767f444bcb24
ibrahimtopcu <ibrahimtopcu193@gmail.com>
2025-06-24 22:51:47 +0300

    Bumpversion testing

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,23 +1,14 @@
-# Configuration file for the Sphinx documentation builder.
-
 import os
 import sys
-from pathlib import Path
-
-# -- Path setup -----------------------------------------------------------
 sys.path.insert(0, os.path.abspath('../../src'))
 
-# -- Project information --------------------------------------------------
 project = 'Tree Height Analysis'
 author = 'Ibrahim Topcu'
 copyright = '2025, Ibrahim Topcu'
 
-# Bu satır __version__ değişkeninden otomatik versiyonu alır
-from tree_height_analysis_57 import __version__
-version = __version__
-release = __version__
+from tree_height_analysis_57 import __version__ as version
+release = version
 
-# -- General configuration ------------------------------------------------
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
@@ -28,11 +19,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 templates_path = ['_templates']
 language = 'en'
 
-# -- Options for HTML output ----------------------------------------------
 html_theme = 'furo'
 html_static_path = ['_static']
 
-# -- Type hints and docstring formatting ----------------------------------
 autodoc_typehints = "description"
 napoleon_use_param = True
 napoleon_use_rtype = False
